e601d3e86a656a33cf03e7ba2d3ce8ebee06a403
Syabib Ash-Shiddiqi <sasfort14@gmail.com>
2023-01-12 11:40:59 +0700

    change input timezone to UTC+8 (Asia/Singapore)

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
 from dotenv import load_dotenv
 from flask import Flask, request
 from flask_apscheduler import APScheduler
@@ -6,14 +6,15 @@ from flask_mail import Mail, Message
 import os
 import psycopg2
 import psycopg2.extras
+import pytz
 
 CREATE_EVENTS_TABLE = (
-    """CREATE TABLE IF NOT EXISTS EVENTS (ID SERIAL PRIMARY KEY, EVENT_ID INTEGER, 
+    """CREATE TABLE IF NOT EXISTS EVENTS (ID SERIAL PRIMARY KEY, EVENT_ID INTEGER,
     EMAIL_SUBJECT TEXT, EMAIL_CONTENT TEXT, TIMESTAMP TIMESTAMP);"""
 )
 
 INSERT_EVENT = (
-    """INSERT INTO EVENTS (EVENT_ID, EMAIL_SUBJECT, EMAIL_CONTENT, TIMESTAMP) 
+    """INSERT INTO EVENTS (EVENT_ID, EMAIL_SUBJECT, EMAIL_CONTENT, TIMESTAMP)
     VALUES (%s, %s, %s, %s);"""
 )
 
@@ -56,6 +57,7 @@ db_url = os.getenv("DATABASE_URL")
 connection = psycopg2.connect(db_url)
 sched = APScheduler()
 mail = Mail(app)
+tz = pytz.timezone('Asia/Singapore')
 
 
 @app.get('/')
@@ -70,17 +72,20 @@ def create_event():
     email_subject = data['email_subject']
     email_content = data['email_content']
     try:
-        timestamp = datetime.strptime(data['timestamp'], '%d-%m-%Y %H:%M')
+        timestamp = tz.localize(
+            datetime.strptime(data['timestamp'], '%d-%m-%Y %H:%M'), is_dst=None
+        )
     except KeyError:
         return {
             "data": data,
-            "message": 'Use this format for timestamp: day-month-year hour:minute',
+            "message": 'Use this format for timestamp: day-month-year hour:minute UTC+8 (Asia/Singapore)',
         }, 400
     with connection:
         with connection.cursor() as cursor:
             cursor.execute(CREATE_EVENTS_TABLE)
             cursor.execute(
                 INSERT_EVENT, (event_id, email_subject, email_content, timestamp))
+    data['timestamp'] = data['timestamp'] + ' UTC+8 (Asia/Singapore)'
     return {
         "data": data,
         "message": "Event created successfully.",
@@ -163,8 +168,11 @@ def check_events():
         recipient_email_list = []
         for recipient in recipient_list:
             recipient_email_list.append(recipient['email'])
+        current_datetime = datetime.now(pytz.UTC)
         for event in event_list:
-            if event['timestamp'] <= datetime.now():
+            event_singapore_time = event['timestamp'].replace(
+                tzinfo=pytz.UTC)
+            if event_singapore_time <= current_datetime:
                 msg = Message(
                     event['email_subject'],
                     sender=os.getenv("SENDER_EMAIL"),
@@ -180,6 +188,6 @@ def check_events():
 if __name__ == '__main__':
 
     sched.add_job(id='check_events', func=check_events,
-                  trigger='interval', minutes=1)
+                  trigger='interval', seconds=5)
     sched.start()
     app.run(debug=True, use_reloader=False)
